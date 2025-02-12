4df3131c0ebd8edccfe2f8b0cb96f37ead17bc8d
Muhammad Zareef <muhammadzareef709@gmail.com>
2025-02-12 15:25:44 +0500

    Uploaded

---
+++ app.js
@@ -0,0 +1,476 @@
+
+let cars = {
+
+    Mercedes: {
+        "Benz G Class": {
+            name: "Mercedes Benz G Class",
+            model: "G 63 AMG",
+            year: 2025,
+            price: "PKR 8.72 crore",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/6159/medium/Mercedes-Benz-G-Class-Front-Right-Angled.jpg?1646405434"
+        },
+        "Benz S Class": {
+            name: "Mercedes Benz S Class",
+            model: "S 450 4MATIC Luxury",
+            year: 2024,
+            price: "PKR 8 crore",
+            url: "https://cache3.pakwheels.com/system/car_generation_pictures/6455/medium/Mercedes_Benz_S-class.jpg?1654147957"
+        },
+        "Benz GLB Class": {
+            name: "Mercedes Benz GLB Class",
+            model: "GLB 200 7-seater",
+            year: 2022,
+            price: "PKR 3.6 crore",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/5619/medium/gb_200_amg_line.jpg?1631007883"
+        },
+        "Benz A Class": {
+            name: "Mercedes Benz A Class",
+            model: "A200",
+            year: 2023,
+            price: "PKR 3 crore",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/6414/medium/A_Class.jfif?1651056396"
+        },
+	},
+
+    BMW: {
+        i4: {
+            name: "BMW i4",
+            model: "M50",
+            year: 2024,
+            price: "PKR 4.2 crore",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/6044/medium/BMW_i4_-_PNG.png?1637071894",
+        },
+        "2 Series": {
+            name: "BMW 2 Series",
+            model: "218i Gran Coupe",
+            year: 2022,
+            price: "PKR 3.2 crore",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/6405/medium/BMW_2_Series_Front_.jpg?1650872649",
+        },
+        iX3: {
+            name: "BMW iX3",
+            model: "M Sport",
+            year: 2025,
+            price: "PKR 3.3 crore",
+            url: "https://cache2.pakwheels.com/system/car_generation_pictures/6043/medium/BMW_iX3_-PNG.png?1637071572",
+        },
+        iX: {
+            name: "BMW iX",
+            model: "xDrive50",
+            year: 2024,
+            price: "PKR 5.4 crore",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/6407/medium/BMW_iX_Front.jpg?1650874000",
+        },
+        i5: {
+            name: "BMW i5",
+            model: "eDrive40 M Sport",
+            year: 2025,
+            price: "PKR 4.52 crore",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/7707/medium/BMW_i5.png?1712564911",
+        },
+        "7 Series": {
+            name: "BMW 7 Series",
+            model: "i7 xDrive60 Excellence",
+            year: 2024,
+            price: "PKR 8.08 crore",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/7284/medium/1.jpg?1672832481",
+        },
+        X5: {
+            name: "BMW X5",
+            model: "xDrive45e",
+            year: 2025,
+            price: "PKR 7.4 crore",
+            url: "https://cache3.pakwheels.com/system/car_generation_pictures/5258/medium/x5.jpg?1595597749",
+        },
+    },
+
+    Audi: {
+        Q2: {
+            name: "Audi Q2",
+            model: "1.0 TFSI Exclusive Line",
+            year: 2024,
+            price: "PKR 72.5 lacs",
+            url: "https://cache4.pakwheels.com/system/car_generation_pictures/6398/medium/Audi_Q2_Front.jpg?1650617857",
+        },
+        "Q8 e-tron": {
+            name: "Audi Q8 e-tron",
+            model: "Sportback 55 quattro",
+            year: 2025,
+            price: "PKR 4.75 crore",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/7812/medium/Cover.jpg?1732025336",
+        },
+        "e-tron GT": {
+            name: "Audi e-tron GT",
+            model: "RS",
+            year: 2025,
+            price: "PKR 8.1 crore",
+            url: "https://cache4.pakwheels.com/system/car_generation_pictures/5519/medium/audi-gt-etron.jpg?1626680748",
+        },
+    },
+
+    Hyundai: {
+        Tucson: {
+            name: "Hyundai Tucson",
+            model: "AWD A/T Ultimate",
+            year: 2024,
+            price: "PKR 87.84 lacs",
+            url: "https://cache3.pakwheels.com/system/car_generation_pictures/6022/medium/Tucson_-_PNG.png?1635948541",
+        },
+        Elantra: {
+            name: "Hyundai Elantra",
+            model: "Hybrid",
+            year: 2025,
+            price: "PKR 97 lacs",
+            url: "https://cache2.pakwheels.com/system/car_generation_pictures/7778/medium/Front_Left_View.jpg?1729860896",
+        },
+        Sonata: {
+            name: "Hyundai Sonata",
+            model: "N line 2.5 Turbo",
+            year: 2025,
+            price: "PKR 1.59 crore",
+            url: "https://cache2.pakwheels.com/system/car_generation_pictures/8025/medium/Cover.jpg?1737612526",
+        },
+        "Santa Fe": {
+            name: "Hyundai Santa Fe",
+            model: "Signature",
+            year: 2024,
+            price: "PKR 1.39 crore",
+            url: "https://cache4.pakwheels.com/system/car_generation_pictures/7593/medium/Santa--fe-cover.jpg?1696188476",
+        },
+        "Ioniq 6": {
+            name: "Hyundai Ioniq 6",
+            model: "EV",
+            year: 2025,
+            price: "PKR 2.3 crore",
+            url: "https://cache2.pakwheels.com/system/car_generation_pictures/7696/medium/Ioniq_6.jpeg?1709630326",
+        },
+        "Ioniq 5": {
+            name: "Hyundai Ioniq 5",
+            model: "EV",
+            year: 2025,
+            price: "PKR 2.5 crore",
+            url: "https://cache4.pakwheels.com/system/car_generation_pictures/7697/medium/ioniq_5_ev.jpg?1709630416",
+        },
+    },
+
+    Porsche: {
+        Taycan: {
+            name: "Porsche Taycan",
+            model: "GTS",
+            year: 2025,
+            price: "PKR 4.75 crore",
+            url: "https://cache2.pakwheels.com/system/car_generation_pictures/6417/medium/Porsche_Taycan_Front.jpg?1651122512"
+        },
+        Cayman: {
+            name: "Porsche Cayman",
+            model: "S",
+            year: 2024,
+            price: "PKR 3.2 crore",
+            url: "https://cache2.pakwheels.com/system/car_generation_pictures/4666/medium/Porsche_Cayman.jpg?1503566332"
+        },
+	},
+
+    Tesla: {
+        "Model Y": {
+            name: "Tesla Model Y",
+            model: "Performance",
+            year: 2025,
+            price: "PKR 3.5 crore",
+            url: "https://cache4.pakwheels.com/system/car_generation_pictures/7640/medium/tesla-model-y.png?1702622968"
+        },
+	},
+
+    Toyota: {
+        Corolla: {
+            name: "Toyota Corolla",
+            model: "Altis Grande X CVT-i",
+            year: 2025,
+            price: "PKR 75.5 lacs",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/5361/original/Corolla-X-Cars-Cropped-Pictures-for-Website.jpg?1606903674",
+        },
+        Yaris: {
+            name: "Toyota Yaris",
+            model: "Ativ X CVT 1.5",
+            year: 2025,
+            price: "PKR 63.2 lacs",
+            url: "https://cache2.pakwheels.com/system/car_generation_pictures/8051/original/Cover.jpg?1737968957",
+        },
+        Cross: {
+            name: "Toyota Cross",
+            model: "Cross",
+            year: 2024,
+            price: "PKR 94.25 lacs",
+            url: "https://cache4.pakwheels.com/system/car_generation_pictures/7741/original/Corolla_Cross_JDM.png?1722320891",
+        },
+        Hilux: {
+            name: "Toyota Hilux",
+            model: "Revo Rocco",
+            year: 2023,
+            price: "PKR 1.44 crore",
+            url: "https://cache2.pakwheels.com/system/car_generation_pictures/6006/original/Revo_-_PNG.png?1635483853",
+        },
+        Fortuner: {
+            name: "Toyota Fortuner",
+            model: "Legender",
+            year: 2024,
+            price: "PKR 1.9 crore",
+            url: "https://cache4.pakwheels.com/system/car_generation_pictures/7335/original/Fortuner_-_PNG.png?1677568997",
+        },
+        Camry: {
+            name: "Toyota Camry",
+            model: "High Grade",
+            year: 2025,
+            price: "PKR 3 crore",
+            url: "https://cache2.pakwheels.com/system/car_generation_pictures/6423/original/Toyota_Camry_Front.jpg?1651129122",
+        },
+        Prado: {
+            name: "Toyota Prado",
+            model: "VX 2.8L D",
+            year: 2023,
+            price: "PKR 7.56 crore",
+            url: "https://cache4.pakwheels.com/system/car_generation_pictures/7711/original/PRADO.jpg?1715249572",
+        },
+        "Land Cruiser": {
+            name: "Toyota Land Cruiser",
+            model: "ZX Gasoline 3.5L",
+            year: 2024,
+            price: "PKR 12 crore",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/6008/original/Land_Cruiser_300_-_PNG.png?1635484577",
+        },
+    },
+
+    Honda: {
+        Civic: {
+            name: "Honda Civic",
+            model: "RS",
+            year: 2025,
+            price: "PKR 98.99 lacs",
+            url: "https://cache4.pakwheels.com/system/car_generation_pictures/7370/original/Cover.jpg?1677570254",
+        },
+        City: {
+            name: "Honda City",
+            model: "1.5L ASPIRE CVT",
+            year: 2023,
+            price: "PKR 58.49 lacs",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/6425/original/Honda_City_Front.jpg?1651424945",
+        },
+        "BR-V": {
+            name: "Honda BR-V",
+            model: "i-VTEC S",
+            year: 2024,
+            price: "PKR 63.0 lacs",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/6430/original/Honda_BRV_Front.jpg?1652159445",
+        },
+        "HR-V": {
+            name: "Honda HR-V",
+            model: "VTi-S",
+            year: 2025,
+            price: "PKR 78.99 lacs",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/7250/medium/f.jpg?1667212296",
+        },
+        Accord: {
+            name: "Honda Accord",
+            model: "1.5L VTEC Turbo",
+            year: 2025,
+            price: "PKR 2.3 crore",
+            url: "https://cache2.pakwheels.com/system/car_generation_pictures/6831/medium/Honda_Accord_Front_Right_angled_.jpg?1663243625",
+        },
+    },
+
+    Suzuki: {
+        Cultus: {
+            name: "Suzuki Cultus",
+            model: "Auto Gear Shift",
+            year: 2025,
+            price: "PKR 45.46 lacs",
+            url: "https://cache4.pakwheels.com/system/car_generation_pictures/6014/medium/Suzuki_Cultus_-_PNG.png?1635945515",
+        },
+        Alto: {
+            name: "Suzuki Alto",
+            model: "VXL AGS",
+            year: 2024,
+            price: "PKR 30.45 lacs",
+            url: "https://cache4.pakwheels.com/system/car_generation_pictures/6013/medium/Suzuki_Alto_-_PNG.png?1635945100",
+        },
+        Swift: {
+            name: "Suzuki Swift",
+            model: "GLX CVT",
+            year: 2025,
+            price: "PKR 47.19 lacs",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/7441/medium/SWIFT.jpg?1677750438",
+        },
+        "Wagon R": {
+            name: "Suzuki Wagon R",
+            model: "AGS",
+            year: 2023,
+            price: "PKR 37.41 lacs",
+            url: "https://cache4.pakwheels.com/system/car_generation_pictures/7314/medium/Wagon-R.jpg?1677147187",
+        },
+    },
+
+    KIA: {
+        EV9: {
+            name: "KIA EV9",
+            model: "GT Line",
+            year: 2025,
+            price: "PKR 4.32 crore",
+            url: "https://cache3.pakwheels.com/system/car_generation_pictures/8137/medium/Cover.jpg?1738825619"
+        },
+        EV5: {
+            name: "KIA EV5",
+            model: "EARTH",
+            year: 2024,
+            price: "PKR 2.35 crore",
+            url: "https://cache3.pakwheels.com/system/car_generation_pictures/7884/medium/Kia_EV5.jpg?1735285514"
+        },
+        Carnival: {
+            name: "KIA Carnival",
+            model: "3.5L V6",
+            year: 2025,
+            price: "PKR 1.75 crore",
+            url: "https://cache3.pakwheels.com/system/car_generation_pictures/7988/medium/Cover.jpg?1737101800"
+        },
+        "Sportage L": {
+            name: "KIA Sportage L",
+            model: "HEV",
+            year: 2025,
+            price: "PKR 1.29 crore",
+            url: "https://cache3.pakwheels.com/system/car_generation_pictures/8102/medium/Cover.jpg?1738231817"
+        },
+        Sorento: {
+            name: "KIA Sorento",
+            model: "3.5 FWD",
+            year: 2024,
+            price: "PKR 94.99 lacs",
+            url: "https://cache3.pakwheels.com/system/car_generation_pictures/6433/medium/KIA_Sorento_Front.jpg?1652180248"
+        },
+        Stonic: {
+            name: "KIA Stonic",
+            model: "EX+",
+            year: 2022,
+            price: "PKR 55 lacs",
+            url: "https://cache4.pakwheels.com/system/car_generation_pictures/5510/medium/kia-stonic-blue-front.jpg?1624447924"
+        },
+        Picanto: {
+            name: "KIA Picanto",
+            model: "1.0 AT",
+            year: 2020,
+            price: "PKR 38.5 lacs",
+            url: "https://cache3.pakwheels.com/system/car_generation_pictures/6017/medium/Picanto_-_PNG.png?1635946684"
+        },
+    },
+
+    MG: {
+        Cyberster: {
+            name: "MG Cyberster",
+            model: "GT (Dual)",
+            year: 2025,
+            price: "PKR 3 crore",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/7782/medium/mg-cyberster-rwd-specs-features-removebg-preview.png?1730096831"
+        },
+        "5 EV": {
+            name: "MG 5 EV",
+            model: "SE Long Range",
+            year: 2025,
+            price: "PKR 1.35 crore",
+            url: "https://cache2.pakwheels.com/system/car_generation_pictures/7658/medium/MG5.png?1705665712"
+        },
+        "MG 4": {
+            name: "MG 4",
+            model: "Essence",
+            year: 2024,
+            price: "PKR 1.1 crore",
+            url: "https://cache1.pakwheels.com/system/car_generation_pictures/7262/medium/1.jpg?1669982857"
+        },
+        "ZS EV": {
+            name: "MG ZS EV",
+            model: "Luxury",
+            year: 2023,
+            price: "PKR 1.02 crore",
+            url: "https://cache4.pakwheels.com/system/car_generation_pictures/5322/medium/zs-ev.jpg?1600344620"
+        },
+        HS: {
+            name: "MG HS",
+            model: "PHEV",
+            year: 2022,
+            price: "PKR 94.99 lacs",
+            url: "https://cache2.pakwheels.com/system/car_generation_pictures/5327/medium/hs.jpg?1600434088"
+        },
+    },
+};
+
+function displayAllBrands(allBrands) {
+    for(let brand in allBrands) {
+        displayBrand(allBrands[brand]);
+    }
+}
+
+displayAllBrands(cars);
+
+function displayModel(model) {
+
+    let cardContainer = document.getElementById("cards-container");
+
+    cardContainer.innerHTML += `
+    <div class="card">
+        <img src="${model.url}" class="card-img-top" alt=">${model.name}">
+        <div class="card-body" id="card-body">
+            <h5 class="card-title">${model.name}</h5>
+            <p class="card-text">${model.model}</p>
+            <p>${model.year}</p>
+            <p>${model.price}</p>
+        </div>
+    </div>
+    `;
+}
+
+function displayBrand(brand) {
+    for(let model in brand) {
+        displayModel(brand[model]);
+    }
+}
+
+let brands = document.getElementById("selectCarBrand");
+
+function handleBrandChange() {
+    let brand = brands.value;
+    removeAllCards();
+    displayBrand(cars[brand]);
+    addModelOptions(cars[brand]);
+}
+
+function handleModelChange() {
+    let models = document.getElementById("selectCarModel");
+    let modelValue = models.value;
+    let brandValue = brands.value;
+    removeAllCards();
+    displayModel(cars[brandValue][modelValue]);
+}
+
+function addBrandOptions(carBrands) {
+
+    let brandOptionId = document.getElementById("selectCarBrand");
+
+    brandOptionId.innerHTML = `<option selected disabled>Select Car Brand</option>`;
+
+    for(let key in carBrands) {
+        brandOptionId.innerHTML += `<option value="${key}">${key}</option>`;
+    }
+}
+
+addBrandOptions(cars);
+
+function addModelOptions(carModels) {
+
+    let modelOptionId = document.getElementById("selectCarModel");
+
+    modelOptionId.innerHTML = `<option selected disabled>Select Car Model</option>`;
+
+    for(let key in carModels) {
+        modelOptionId.innerHTML += `<option value="${key}">${key}</option>`;
+    }
+}
+
+function removeAllCards() {
+    document.getElementById("cards-container").innerHTML = "";
+}
